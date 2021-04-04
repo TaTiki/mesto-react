f17bf45fd6d95d659f1ffde8f4b5a8423baeed0d
tatiana_kirsanova <alisia7788@mail.ru>
2021-04-04 19:43:19 +0300

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -8,8 +8,13 @@ import { CurrentUserContext } from "../contexts/CurrentUserContext";
 import EditProfilePopup from './EditProfilePopup';
 import EditAvatarPopup from './EditAvatarPopup';
 import AddPlacePopup from './AddPlacePopup';
+import { Switch, Route, withRouter, useLocation } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+import ProtectedRoute from "./ProtectedRoute";
+import auth, { Auth } from '../utils/auth';
 
-function App() {
+function App({history}) {
 
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
@@ -17,17 +22,94 @@ function App() {
   const [selectedCard, setSelectedCard] = useState({});
   const [currentUser, setCurrentUser] = useState({});
   const [cards, setCards] = useState([]);
+  const [JWT, setJWT] = useState('');
+  const [email, setEmail] = useState('');
+  const [headerProps, setHeaderProps] = useState({buttonText: '', onClick: () => {}});
 
+  const { pathname } = useLocation();
 
   useEffect(() => {
-    Promise.all([
-      api.getUser(),
-      api.getInitialCards(),
-    ]).then(([user, cards]) => {
-      setCurrentUser(user);
-      setCards(cards);
-    }).catch(console.log);
-  }, [])
+    console.log(pathname)
+    switch (pathname) {
+      case '/':
+        console.log(1) 
+        setHeaderProps({
+          buttonText: 'Выйти',
+          onClick: () => {
+            localStorage.removeItem('JWT');
+            setEmail('');
+          }
+        });
+        
+        break;
+      case '/sign-in': 
+      console.log(2)
+        setHeaderProps({
+          buttonText: 'Регистрация',
+          onClick: () => {
+            history.push('/sign-up');
+          }
+        });
+        
+        break;
+      case '/sign-up': 
+      console.log(3)
+        setHeaderProps({
+          buttonText: 'Войти',
+          onClick: () => {
+            history.push('/sign-in');
+          }
+        });
+    }
+  },[pathname] )
+
+  useEffect(() => {
+    console.log('babis')
+    console.log(localStorage.getItem('JWT'))
+    
+    handleTokenCheck()
+    
+   
+  }, [JWT])
+
+  useEffect(() => {
+    if(email) {
+      
+      Promise.all([
+        api.getUser(),
+        api.getInitialCards(),
+      ]).then(([user, cards]) => {
+        setCurrentUser(user);
+        setCards(cards);
+      }).catch(console.log);
+    }
+  }, [email])
+
+  const handleTokenCheck = () => {
+    if(localStorage.getItem('JWT')) {
+      const JWT = localStorage.getItem('JWT');
+      auth.getAuthUser(JWT).then((email) => {
+        setEmail(email);
+        history.push('/');
+      }).catch(console.log)
+    }
+  }
+
+  const handleSignUp = (email, password) => {
+    console.log('babis')
+    auth.signup(email, password).then(()=> {
+      history.push('/sign-in')
+    })
+    .catch(console.log)
+  }
+
+  const handleSignIn = (email, password) => {
+    auth.signin(email, password).then((token) => {
+      console.log(token)
+      setJWT(token);
+     
+    }).catch(console.log)
+  }
 
   const handleEditAvatarClick = () => {
     setIsEditAvatarPopupOpen(true);
@@ -83,10 +165,18 @@ function App() {
   return (
     <CurrentUserContext.Provider value={currentUser}>
       <div className="page">
-        <Header/>
-        <Main onEditProfile={handleEditProfileClick}
-        onAddPlace={handleAddPlaceClick} onEditAvatar={handleEditAvatarClick}
-        onCardClick={handleCardClick} cards = {cards} onCardLike = {handleCardLike} onCardDelete = {handleCardDelete}/>
+        <Header email = {email} onClick = {headerProps.onClick} textButton = {headerProps.buttonText} />
+        <Switch>
+          <ProtectedRoute exact path = '/' component = { Main } onEditProfile={handleEditProfileClick}
+            onAddPlace={handleAddPlaceClick} onEditAvatar={handleEditAvatarClick} loggedIn = {email !== ''}
+            onCardClick={handleCardClick} cards = {cards} onCardLike = {handleCardLike} onCardDelete = {handleCardDelete}/>
+          <Route path = '/sign-up'>
+            <Register onSubmit = {handleSignUp}/>
+          </Route>  
+          <Route path = '/sign-in'>
+            <Login onSubmit = {handleSignIn}/>
+          </Route>  
+        </Switch>
         <EditProfilePopup isOpen ={isEditProfilePopupOpen} onClose ={closeAllPopups} onUpdateUser = {handleUpdateUser}/> 
         <EditAvatarPopup isOpen ={isEditAvatarPopupOpen} onClose ={closeAllPopups} onUpdateAvatar = {handleUpdateAvatar}/> 
         <AddPlacePopup isOpen = {isAddPlacePopupOpen} onClose = {closeAllPopups} onAddPlace = {handleAddPlace}/>
@@ -99,4 +189,4 @@ function App() {
   );
 }
 
-export default App;
+export default withRouter(App);
